b90a6ed9190381d97e5457eba438a8010457e70f
kmicaela20-ui <kmicaela20@gmail.com>
2026-05-13 19:44:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -778,3 +778,10 @@ init()
 
 if __name__=='__main__':
     app.run(host='0.0.0.0',port=int(os.environ.get('PORT',5000)))
+
+
+
+init()
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0',port=int(os.environ.get('PORT',5000)))
